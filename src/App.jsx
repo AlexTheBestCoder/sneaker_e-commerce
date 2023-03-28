976b457a70fe0eaa418476b4082beba3e151a539
AlexTheBestCoder <thebestcoder10@gmail.com>
2023-03-28 20:35:23 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import CardMain from './CardMain'
+import Navbar from './Navbar'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Navbar/>
+      <CardMain/>
+    </div>
+  )
+}
+
+export default App
