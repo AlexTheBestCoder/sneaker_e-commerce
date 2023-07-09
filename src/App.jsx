82848d31716ebf5bbb6b04b22447e02990d3cac5
AlexTheBestCoder <thebestcoder10@gmail.com>
2023-07-09 14:11:27 +0000

    V1

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import React, { useContext} from "react";
+import React, { useContext } from "react";
 import "./App.css";
-import Home from "./pages/Home"
+import Home from "./pages/Home";
 import { ThemeContext } from "./context/ThemeContext";
 // import {CountProvider} from "./CountContext";
 
 function App() {
   const { isDarkMode } = useContext(ThemeContext);
-  
+  const body = document.querySelector("body");
+  if (isDarkMode) {
+    body.style.backgroundColor = "#282837";
+    body.style.color = "#222";
+    body.style.fill = "white";
+  } else {
+    body.style.backgroundColor = "#fff";
+    body.style.color = "#222";
+  }
+
   return (
-    <div className={`App ${isDarkMode ? "dark" : "light"}`}>
-     <Home />
+    <div className={`App ${isDarkMode ? 'dark': 'light'}`}>
+      <Home />
     </div>
   );
 }
